808a96fd3e195123f1a5152882b6d73ac33b38ba
Ben Herila <ben@herila.net>
2020-05-17 23:31:28 -0700

    Updates

---
+++ precache-manifest.0b745a1327796c52b3ba0424731b0edb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4842b96c2e61a084034ce42537cf13a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a4248abb82b9437e80b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d72327532853a4b9e682",
+    "url": "/static/js/2.438c4234.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.438c4234.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a4248abb82b9437e80b",
+    "url": "/static/js/main.84cc1b76.chunk.js"
+  },
+  {
+    "revision": "52fd2119b6f0b4fb0cbe",
+    "url": "/static/js/runtime-main.416f9079.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
